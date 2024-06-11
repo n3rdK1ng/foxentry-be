41da7388c6ff52f1e147a0a25fd538181203a028
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-06-11 23:08:02 +0200

    chore: add environment variable validation (#2)

---
+++ src/env.validation.ts
@@ -0,0 +1,30 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNotEmpty, IsString, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+  @IsString()
+  @IsNotEmpty()
+  ELASTIC_URL: string;
+
+  @IsString()
+  @IsNotEmpty()
+  ELASTIC_USERNAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  ELASTIC_PASSWORD: string;
+}
+
+export const validate = (config: Record<string, unknown>) => {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+};
