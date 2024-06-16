20ef530287cc6f1e92b00a189078350c020a573a
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-06-16 19:35:26 +0200

    chore: vercel deploy config (#20)

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document, {
     customSiteTitle: 'Foxentry API Documentation',
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
   })
 
   await app.listen(3000)
