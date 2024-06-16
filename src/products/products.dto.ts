07d6e7fd9e75930dc7dc6018c80f2af6d839db96
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-06-16 11:32:54 +0200

    docs: add swagger openapi (#17)

--- src/products/products.dto.ts
+++ src/products/products.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class ProductDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string
 
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   price: number
 
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   stock: number
